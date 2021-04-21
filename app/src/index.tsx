219ffe86c8a237f27c629388fa9925c515f6aa87
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-20 20:47:45 -0400

    service obj

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,41 @@
 import ReactDOM from 'react-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-	return <h1>hi, i'm app</h1>;
+	const service = useRef<any>(null);
+
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const handleClick = () => {
+		if (!service.current) return;
+		console.log(service.current);
+	};
+
+	const startService = async (): Promise<any> => {
+		service.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	return (
+		<div>
+			<textarea
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+			></textarea>
+			<div>
+				<button onClick={handleClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
