28ef36267ed2548b8a5dc8e306a9f9e0cafca3d3
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-21 21:56:22 -0400

    css workaround

--- app/src/plugins/fetch-plugin.tsx
+++ app/src/plugins/fetch-plugin.tsx
@@ -10,14 +10,44 @@ export const fetchPlugin = (clientInput: string) => {
 	return {
 		name: 'fetch-plugin',
 		setup(build: esbuild.PluginBuild) {
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				if (args.path === 'index.js') {
-					return {
-						loader: 'jsx',
-						contents: clientInput,
-					};
-				}
+			//
+			build.onLoad({ filter: /(^index\.js$)/ }, () => {
+				return {
+					loader: 'jsx',
+					contents: clientInput,
+				};
+			});
 
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+				const inCache = await cache.getItem<esbuild.OnLoadResult>(args.path);
+
+				if (inCache) return inCache;
+
+				const { data, request } = await axios.get(args.path);
+
+				const escaped = data
+					.replace(/\n/g, '')
+					.replace(/"/g, '\\"')
+					.replace(/'/g, "\\'");
+
+				const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style); 
+          `;
+
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents,
+					resolveDir: new URL('./', request.responseURL).pathname,
+				};
+
+				await cache.setItem(args.path, result);
+
+				return result;
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				const inCache = await cache.getItem<esbuild.OnLoadResult>(args.path);
 
 				if (inCache) return inCache;
