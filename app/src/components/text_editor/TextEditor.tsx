9b38cf5348df79fb1b0263f1c8b25b61e7f0ba65
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-18 17:55:05 -0400

    toggle editor

--- app/src/components/text_editor/TextEditor.tsx
+++ app/src/components/text_editor/TextEditor.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
-	return (
+	const [editing, setEditing] = useState(false);
+
+	useEffect(() => {
+		const listener = () => setEditing(false);
+		document.addEventListener('click', listener, { capture: true });
+		return () =>
+			document.removeEventListener('click', listener, { capture: true });
+	}, []);
+
+	return editing ? (
 		<div>
 			<MDEditor />
+		</div>
+	) : (
+		<div onClick={() => setEditing(true)}>
+			<MDEditor.Markdown source={'# Header'} />
 		</div>
 	);
 };
