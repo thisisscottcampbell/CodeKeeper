d69a142b647f4566f71c39e7bfb007578e23f286
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-25 14:57:14 -0400

    toggle display

--- app/src/components/text_editor/TextEditor.tsx
+++ app/src/components/text_editor/TextEditor.tsx
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+	const ref = useRef<HTMLDivElement | null>(null);
 	const [editing, setEditing] = useState(false);
 
 	useEffect(() => {
-		const listener = () => setEditing(false);
+		const listener = (e: MouseEvent) => {
+			if (ref.current && e.target && ref.current.contains(e.target as Node))
+				return;
+			setEditing(false);
+		};
 		document.addEventListener('click', listener, { capture: true });
 		return () =>
 			document.removeEventListener('click', listener, { capture: true });
 	}, []);
 
 	return editing ? (
-		<div>
+		<div ref={ref}>
 			<MDEditor />
 		</div>
 	) : (
