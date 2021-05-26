e2b76437283547a586fc21575c4de682a78398ac
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-25 23:51:12 -0400

    update reducer

--- app/src/state/reducers/cellsReducer.ts
+++ app/src/state/reducers/cellsReducer.ts
@@ -1,36 +1,48 @@
-import { sType } from '../s-types',
+import { ActionType } from '../action-types';
 import { Actions } from '../actions';
 import { Cell } from '../cell';
 
 interface CellState {
-  loading: boolean;
-  error: string | null;
-  order: string[];
-  data: {
-    [key: string]: Cell
-  }
+	loading: boolean;
+	error: string | null;
+	order: string[];
+	data: {
+		[key: string]: Cell;
+	};
 }
 
 const initialState: CellState = {
-  loading: false,
-  error: null,
-  order: [],
-  data: {}
-}
+	loading: false,
+	error: null,
+	order: [],
+	data: {},
+};
 
-const reducer = (state: CellState = initialState, action: Actions): CellState => {
-  switch (action.type) {
-    case sType.UPDATE_CELL:
-      return state;
-    case sType.DELETE_CELL:
-      return state;
-    case sType.MOVE_CELL:
-      return state;
-    case sType.INSERT_CELL_BEFORE:
-      return state;
-    default:
-      return state;
-  }
-}
+const reducer = (
+	state: CellState = initialState,
+	action: Actions
+): CellState => {
+	switch (action.type) {
+		case ActionType.UPDATE_CELL:
+			return {
+				...state,
+				data: {
+					...state.data,
+					[action.payload.id]: {
+						...state.data[action.payload.id],
+						content: action.payload.content,
+					},
+				},
+			};
+		case ActionType.DELETE_CELL:
+			return state;
+		case ActionType.MOVE_CELL:
+			return state;
+		case ActionType.INSERT_CELL_BEFORE:
+			return state;
+		default:
+			return state;
+	}
+};
 
 export default reducer;
